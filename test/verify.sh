56291c43292f0584b25f832e4a116e1a1d165a60
Isaac Jurado <diptongo@gmail.com>
2009-09-20 04:31:38 +0200

    [svn r66] Add forgotten test results for debug comparisons.

--- test/verify.sh
+++ test/verify.sh
@@ -8,27 +8,20 @@ FPREFIX=fail
 #
 #     $1 --> executed binary
 #     $2 --> test data
-#     $3 --> distance type
-#     $4 --> data type
-#     $5 --> scalar (optional)
+#     $3 --> data type
+#     $4 --> scalar (optional)
 #
 check()
 {
     if [ $? -ne 0 ]
     then
         echo "NOT COMPLETED"
-    elif cmp -s result-$2.$3 $RFILE
+    elif cmp -s result-$2 $RFILE
     then
         echo "OK"
     else
-        dumpfile=$FPREFIX-$2-$1-$4${5+-scalar}.$3
-        if [ $3 = "manhattan" ]
-        then
-                m=--manhattan
-        else
-                m=
-        fi
-        ../debug/$1 --runs=1 $m --type=$4 ${5+--scalar} $2 2>&1 | awk -f ../debug/filter.awk >$dumpfile
+        dumpfile=$FPREFIX-$2-$1-$3${4+-scalar}
+        ../debug/$1 --runs=1 --type=$3 ${4+--scalar} $2 2>&1 | awk -f ../debug/filter.awk >$dumpfile
         echo "FAILED --> $dumpfile"
     fi
 }
@@ -51,26 +44,13 @@ run_test()
         printf "$t "
         printf "scalar  "
         ../$1 --output=$RFILE --type=$t --scalar $2 >/dev/null 2>&1
-        check $1 $2 euclid $t scalar
+        check $1 $2 $t scalar
         printf "                            vector  "
         ../$1 --output=$RFILE --type=$t $2 >/dev/null 2>&1
-        check $1 $2 euclid $t
+        check $1 $2 $t
         printf "                     "
     done
     echo
-#    printf "  Manhattan distance "
-#    for t in "byte  "  "short "  "int   "  "float "  "double"
-#    do
-#        printf "$t "
-#        printf "scalar  "
-#        ../$1 --manhattan --output=$RFILE --type=$t --scalar $2 >/dev/null 2>&1
-#        check $1 $2 manhattan $t scalar
-#        printf "                            vector  "
-#        ../$1 --manhattan --output=$RFILE --type=$t $2 >/dev/null 2>&1
-#        check $1 $2 manhattan $t
-#        printf "                     "
-#    done
-#    echo
     echo
 }
 
