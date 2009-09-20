f2986663ecbc0aadf2d422fcc720cbc2d91c9122
Isaac Jurado <diptongo@gmail.com>
2009-09-20 04:29:32 +0200

    [svn r34] Separated the output filtering for easier individual testing.
    The checker script tests all NN versions, in order to obtain details about
    implementations separately, the AWK filter has been extracted from it.

--- debug/check_versions.sh
+++ debug/check_versions.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-filter='$1 == "[DEBUG" && $7 == "." { printf "%s\t%s\t%s\n", $4, $5, $6 }'
-
 #
 # Euclid versions
 #
@@ -9,7 +7,7 @@ for type in byte short int float double
 do
         echo -n "Verifying Euclid, scalar, $type... "
         ./bench --runs 1 --type $type --scalar ../test/basic 2>&1 | \
-        awk "$filter" >result_check
+        awk -f filter.awk >result_check
 
         if cmp -s ../test/debug.euclid result_check
         then
@@ -20,7 +18,7 @@ do
 
         echo -n "Verifying Euclid, vectorized, $type... "
         ./bench --runs 1 --type $type ../test/basic 2>&1 | \
-        awk "$filter" >result_check
+        awk -f filter.awk >result_check
 
         if cmp -s ../test/debug.euclid result_check
         then
@@ -37,7 +35,7 @@ for type in byte short int
 do
         echo -n "Verifying Manhattan, scalar, $type... "
         ./bench --manhattan --runs 1 --type $type --scalar ../test/basic 2>&1 | \
-        awk "$filter" >result_check
+        awk -f filter.awk >result_check
 
         if cmp -s ../test/debug.manhattan result_check
         then
@@ -48,7 +46,7 @@ do
 
         echo -n "Verifying Manhattan, vectorized, $type... "
         ./bench --manhattan --runs 1 --type $type ../test/basic 2>&1 | \
-        awk "$filter" >result_check
+        awk -f filter.awk >result_check
 
         if cmp -s ../test/debug.manhattan result_check
         then
