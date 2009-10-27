f7218aae679d5b8d328e5851d43e94b5fcf174f1
Isaac Jurado <diptongo@gmail.com>
2009-10-27 19:14:03 +0100

    New plot types: mflops and scalability.
    These new plot types reflect the recent additions to the code (MFLOPS
    calculations and OpenMP parallelization).
    
    Plots have also been redistributed into subdirectories, because of the high
    amount of image files generated.

--- scripts/plot.sh
+++ scripts/plot.sh
@@ -1,9 +1,17 @@
 #!/bin/sh
 
 P=`dirname $0`/..
-test -d $P/plots || mkdir $P/plots
+for d in sizes times mflops scales
+do
+    test -d $P/plots/$d || mkdir -p $P/plots/$d
+done
 
-files=`ls $P/sample_nn_data/*.tst | sed 's/\.tst$//'`
+if [ -f $P/scripts/datafiles ]
+then
+    . $P/scripts/datafiles
+else
+    files=`ls $P/sample_nn_data/*.tst | sed 's/\.tst$//'`
+fi
 
 for f in $files
 do
@@ -21,10 +29,10 @@ do
     #
     # Plot sizes
     #
-    data=$P/plots/sizes-$title.txt
+    data=$P/plots/sizes/table-$title.txt
     for t in $types
     do
-        input=$P/outputs/$title-simple-vec-b0-$t
+        input=$P/outputs/$title-simple-vec-b0-$t-1
         if [ -f $input ]
         then
             values=`awk '/^ +Data array is/ { printf "  " $4 }' $input`
@@ -35,7 +43,7 @@ do
     done >$data
     gnuplot - $P/scripts/sizes.gpi >/dev/null 2>&1 <<EOP
         plot_title  = "$title"
-        plot_output = "$P/plots/$title"
+        plot_output = "$P/plots/sizes/$title"
         plot_data   = "$data"
 EOP
     if [ $? -ne 0 ]
@@ -47,7 +55,7 @@ EOP
     #
     # Plot times
     #
-    data=$P/plots/times-$title.txt
+    data=$P/plots/times/table-$title.txt
     for t in $types
     do
         line="$t"
@@ -57,7 +65,7 @@ EOP
             do
                 for impl in simple unroll2 unroll4
                 do
-                    input=$P/outputs/$title-$impl-$mode-b$bmegs-$t
+                    input=$P/outputs/$title-$impl-$mode-b$bmegs-$t-1
                     if [ -f $input ]
                     then
                         time=`awk '$2 == "Minimum" { print $4 }' $input`
@@ -72,13 +80,108 @@ EOP
     done >$data
     gnuplot - $P/scripts/times.gpi >/dev/null 2>&1 <<EOP
         plot_title  = "$title"
-        plot_output = "$P/plots/$title"
+        plot_output = "$P/plots/times/$title"
         plot_data   = "$data"
 EOP
     if [ $? -ne 0 ]
     then
         printf '!'
     fi
-    printf "times\n"
+    printf 'times  '
+
+    #
+    # Plot MFLOPS
+    #
+    data=$P/plots/mflops/table-$title.txt
+    for t in $types
+    do
+        line="$t"
+        for bmegs in 0 4
+        do
+            for mode in sca vec
+            do
+                for impl in simple unroll2 unroll4
+                do
+                    input=$P/outputs/$title-$impl-$mode-b$bmegs-$t-1
+                    if [ -f $input ]
+                    then
+                        mflops=`awk '$2 == "Minimum" { print substr($6, 2) }' $input`
+                    else
+                        mflops='-'
+                    fi
+                    line="$line  $mflops"
+                done
+            done
+        done
+        echo "$line"
+    done >$data
+    gnuplot - $P/scripts/mflops.gpi >/dev/null 2>&1 <<EOP
+        plot_title  = "$title"
+        plot_output = "$P/plots/mflops/$title"
+        plot_data   = "$data"
+EOP
+    if [ $? -ne 0 ]
+    then
+        printf '!'
+    fi
+    printf 'mflops  '
+
+    #
+    # Plot scalability
+    #
+    data=$P/plots/scales/table-$title
+    for bmegs in 0 4
+    do
+        for threads in 1 2 3 4 5 6 7 8
+        do
+            line=''
+            for t in $types
+            do
+                for mode in sca vec
+                do
+                    for impl in simple unroll2 unroll4
+                    do
+                        input=$P/outputs/$title-$impl-$mode-b$bmegs-$t-$threads
+                        if [ -f $input ]
+                        then
+                            time=`awk '$2 == "Minimum" { print $4 }' $input`
+                        else
+                            time='-'
+                        fi
+                        line="$time  $line"
+                    done
+                done
+            done
+            echo "$threads  $line"
+        done | awk '$1 == 1 {
+                        printf "1"
+                        for (i = 2;  i <= NF;  i++)
+                        {
+                            printf "  1"
+                            base[i] = $i
+                        }
+                        printf "\n"
+                        next
+                    }
+                    {
+                        printf "%d", $1
+                        for (i = 2;  i <= NF;  i++)
+                            if (base[i] == "-" || $i == "-")
+                                printf "  -"
+                            else
+                                printf "  %f", base[i] / $i
+                        printf "\n"
+                    }' >$data-$bmegs.txt
+    done
+    gnuplot - $P/scripts/scales.gpi >/dev/null 2>&1 <<EOP
+        plot_title  = "$title"
+        plot_output = "$P/plots/scales/$title"
+        plot_data   = "$data"
+EOP
+    if [ $? -ne 0 ]
+    then
+        printf '!'
+    fi
+    printf "scales\n"
 done
 
