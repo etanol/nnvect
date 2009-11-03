62ca86346b91f623e4daae403385e456587bc8f3
Isaac Jurado <diptongo@gmail.com>
2009-11-03 17:43:46 +0100

    New script to control all CPU scaling governors at once.

---
+++ scripts/governors.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+sys=/sys/devices/system/cpu
+
+if [ $# -gt 0 ]
+then
+    if fgrep -q $1 $sys/cpu0/cpufreq/scaling_available_governors
+    then
+        echo Using governor $1
+    else
+        echo Governor $1 does not exist
+        exit 2
+    fi
+
+    for cpu in $sys/cpu?
+    do
+        echo $1 > $cpu/cpufreq/scaling_governor
+    done
+else
+    for cpu in $sys/cpu?
+    do
+        gov=`cat $cpu/cpufreq/scaling_governor`
+        echo `basename $cpu`: $gov
+    done
+fi
+
