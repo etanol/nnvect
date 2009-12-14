c61923d982c347f994633106e83f9f1fc08dbdf8
Isaac Jurado <diptongo@gmail.com>
2009-12-14 18:57:23 +0100

    Separate automatic rule generation for future reuse.

--- Makefile
+++ Makefile
@@ -9,28 +9,5 @@ simple_SOURCES := $(common_sources) nnsca-simple.c nnvec-simple.c
 unroll2_SOURCES := $(common_sources) nnsca-unroll2.c nnvec-unroll2.c
 unroll4_SOURCES := $(common_sources) nnsca-unroll4.c nnvec-unroll4.c
 
-
-all: $(PROGRAMS)
-
-define gen_rules
-depfiles := $(patsubst %.c,.%.d,$($(1)_SOURCES))
-objects  += $(patsubst %.c,%.o,$($(1)_SOURCES))
-
-$(1): $(patsubst %.c,%.o,$($(1)_SOURCES))
-	@echo " Linking    $$@" && $(CC) $(LDFLAGS) -o $$@ $$^
-endef
-
-$(foreach p,$(PROGRAMS),$(eval $(call gen_rules,$(p))))
-
-.SUFFIXES: .c .o
-%.o: %.c
-	@echo " Compiling  $@" && $(CC) -pipe -Wall -MMD -MF .$(<F:.c=.d) $(CFLAGS) -c -o $@ $<
-
-
-.PHONY: clean
-clean:
-	@-echo " Cleaning" ; rm -f $(PROGRAMS) $(depfiles) $(objects)
-
-
--include $(depfiles)
+include auto.mk
 
---
+++ auto.mk
@@ -0,0 +1,29 @@
+                     #####################################
+                     #                                   #
+                     #  Automatic build rule generation  #
+                     #                                   #
+                     #####################################
+
+all: $(PROGRAMS)
+
+define build_program
+depfiles += $(patsubst %.c,.%.d,$($(1)_SOURCES))
+objects  += $(patsubst %.c,%.o,$($(1)_SOURCES))
+
+$(1): $(patsubst %.c,%.o,$($(1)_SOURCES))
+	@echo " Linking    $$@" && $(CC) $(LDFLAGS) -o $$@ $$^
+endef
+
+$(foreach p,$(PROGRAMS),$(eval $(call build_program,$(p))))
+
+.SUFFIXES: .c .o
+%.o: %.c
+	@echo " Compiling  $@" && $(CC) -pipe -Wall -MMD -MF .$(<F:.c=.d) $(CFLAGS) -c -o $@ $<
+
+.PHONY: clean
+clean:
+	@-echo " Cleaning" ; rm -f $(PROGRAMS) $(depfiles) $(objects)
+
+
+-include $(depfiles)
+
